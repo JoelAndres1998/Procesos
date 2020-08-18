96c4d764feb0024d2e71a79dc0586a95c857b6f4
JoelAndres1998 <69679009+JoelAndres1998@users.noreply.github.com>
2020-08-18 17:06:58 -0500

    Add files via upload

---
+++ Nube.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+from wordcloud import WordCloud, STOPWORDS
+
+url = 'https://es.stackoverflow.com/users/'
+usuario = raw_input("Ingrese Usuario: ")
+url = url + str(usuario) + "?tab=tags"
+pagina = requests.get(url)
+
+sopa = BeautifulSoup(pagina.content, 'html.parser')
+
+user = sopa.find_all('a', class_='post-tag')
+
+nombres = list()
+
+for i in user:
+    nombres.append(i.text)
+print(nombres)
+
+numeros = sopa.find_all('span', class_='item-multiplier-count')
+numero = list()
+
+for i in numeros:
+    numero.append(i.text)
+print(numero)
+
+with open('nube.txt', 'w') as file:
+    file.write(str(nombres))
+
+comment_words = ' '
+stopwords = ['k', "'", """'"""]
+
+abrir = open('nube.txt', 'r+')
+data = abrir.read().replace('\n', '')
+
+wordcloud = WordCloud(width=700, height=700,
+                      background_color='white', stopwords=stopwords,
+                      min_font_size=10, max_font_size=300).generate(data)
+
+wordcloud.to_file('imagen.png')
+print('imagen guardada')
