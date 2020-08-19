70b9b6c746ef800382240ebba64ff2140c50ec97
JoelAndres1998 <69679009+JoelAndres1998@users.noreply.github.com>
2020-08-18 23:44:37 -0500

    Add files via upload

--- nube/Nube.py
+++ nube/Nube.py
@@ -1,5 +1,5 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 from wordcloud import WordCloud, STOPWORDS
 
 url = 'https://es.stackoverflow.com/users/'
@@ -8,7 +8,6 @@ url = url + str(usuario) + "?tab=tags"
 pagina = requests.get(url)
 
 sopa = BeautifulSoup(pagina.content, 'html.parser')
-
 user = sopa.find_all('a', class_='post-tag')
 
 nombres = list()
@@ -17,25 +16,16 @@ for i in user:
     nombres.append(i.text)
 print(nombres)
 
-numeros = sopa.find_all('span', class_='item-multiplier-count')
-numero = list()
-
-for i in numeros:
-    numero.append(i.text)
-print(numero)
-
-with open('nube.txt', 'w') as file:
+with open('nube.txt','w+') as file:
     file.write(str(nombres))
 
-comment_words = ' '
-stopwords = ['k', "'", """'"""]
+stopwords =[]
 
-abrir = open('nube.txt', 'r+')
-data = abrir.read().replace('\n', '')
+abrir = open('nube.txt','r+')
+data = abrir.read().replace('','')
 
-wordcloud = WordCloud(width=700, height=700,
-                      background_color='white', stopwords=stopwords,
-                      min_font_size=10, max_font_size=300).generate(data)
+wordcloud = WordCloud(width=700,height=700,background_color='cyan',stopwords=stopwords,min_font_size=10, max_font_size=300).generate(data)
 
 wordcloud.to_file('imagen.png')
-print('imagen guardada')
+print('Imagen Generada')
+print('Imagen Guardada')
